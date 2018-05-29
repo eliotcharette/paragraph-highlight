031f62e8b618d027e2e417e38ab8596f75fb9ae9
eliotcharette <34100491+eliotcharette@users.noreply.github.com>
2018-05-29 16:47:14 -0700

    Add files via upload

---
+++ boring_lecture/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".p1").click(function() {
+    $("div").removeClass("border");
+    $(".p1").addClass("border");
+  });
+
+  $(".p2").click(function() {
+    $("div").removeClass("border");
+    $(".p2").addClass("border");
+  });
+});
